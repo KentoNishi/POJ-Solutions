b1aefb70ecbdaca3dab37ef6817b14ec2cf01149
Kento Nishi <kento24gs@outlook.com>
2020-02-06 23:01:47 -0800

    Getting TLE!

--- solutions/reading/main.cpp
+++ solutions/reading/main.cpp
@@ -10,35 +10,38 @@
 using namespace std;
 
 int main() {
-    int P;
-    cin >> P;
-    vector<int> pages = vector<int>(P);
-    set<int> bookTopics;
-    for (int i = 0; i < P; i++) {
+    int N;
+    cin >> N;
+    vector<int> pages = vector<int>(N);
+    set<int> allTopics;
+    for (int i = 0; i < N; i++) {
         cin >> pages[i];
-        bookTopics.insert(pages[i]);
+        allTopics.insert(pages[i]);
     }
-    int numTopics = bookTopics.size();
-    map<int, int> topics;
-    int ans = P;
+    int numTopics = allTopics.size();
     int left = 0;
     int right = 0;
-    while (left < P) {
-        while (right < P && topics.size() < numTopics) {
+    int ans = N;
+    map<int, int> topics;
+    while (left < N) {
+        while (right < N && topics.size() != numTopics) {
             topics[pages[right]]++;
             right++;
         }
-        if (right < P) {
+        if (right == N && topics.size() != numTopics) {
+            break;
+        }
+        right--;
+        // cout << left << " - " << right << " has " << topics.size() << " topics" << endl;
+        if (topics.size() == numTopics) {
             ans = min(ans, right - left + 1);
-        } else {
-            right = P - 1;
-            topics[pages[right]]--;
         }
         topics[pages[left]]--;
         if (topics[pages[left]] == 0) {
             topics.erase(pages[left]);
         }
         left++;
+        right++;
     }
     cout << ans << endl;
     return 0;
