0bc3d74b453861a671100367f2ed8ce99406fc68
Kento Nishi <kento24gs@outlook.com>
2020-02-06 22:24:04 -0800

    Wrong answer!

--- solutions/reading/main.cpp
+++ solutions/reading/main.cpp
@@ -3,10 +3,36 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 using namespace std;
 
 int main() {
+    int P;
+    cin >> P;
+    vector<int> pages = vector<int>(P);
+    set<int> topics;
+    for (int i = 0; i < P; i++) {
+        cin >> pages[i];
+        topics.insert(pages[i]);
+    }
+    int numTopics = topics.size();
+    topics = set<int>();
+    int ans = P;
+    int left = 0;
+    int right = 0;
+    while (left < P) {
+        while (right < P && topics.size() < numTopics) {
+            topics.insert(pages[right]);
+            right++;
+        }
+        if (right < P) {
+            ans = min(ans, right - left);
+        }
+        topics.erase(pages[left]);
+        left++;
+    }
+    cout << ans << endl;
     return 0;
 }
